093d041b378aa10e88beca4ada6cf939874a0e9a
Patrick Allen <patrickallen1305@gmail.com>
2024-05-14 16:51:32 -0500

    bug fixes

--- backend/db/seeders/20240514152730-demo-user.js
+++ backend/db/seeders/20240514152730-demo-user.js
@@ -39,7 +39,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
+    return await queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
     }, {});
   }
