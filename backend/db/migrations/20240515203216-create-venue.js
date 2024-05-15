e63ae37169213fdabc2997f5ff3b3a6a589b6382
Patrick Allen <patrickallen1305@gmail.com>
2024-05-15 17:15:09 -0500

    added models/migrations

---
+++ backend/db/migrations/20240515203216-create-venue.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Venues', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      groupId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Groups" }
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lat: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      lng: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Venues');
+  }
+};
---
+++ backend/db/models/attendance.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Attendance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here,
+      Attendance.hasOne(models.Event, { foreignKey: models.Event.id })
+    }
+  }
+  Attendance.init({
+    eventId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    status: DataTypes.ENUM
+  }, {
+    sequelize,
+    modelName: 'Attendance',
+  });
+  return Attendance;
+};
