56b54ebd4dfb12e006a7a8a4fb9ef4a78a0a6044
Patrick Allen <patrickallen1305@gmail.com>
2024-05-16 10:25:53 -0500

    fixing model references

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -11,13 +11,33 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here,
-      Attendance.hasOne(models.Event, { foreignKey: models.Event.id })
+      Attendance.belongsTo(
+        models.Event,
+        {
+          foreignKey: 'eventId'
+        })
+      Attendance.belongsTo(
+        models.User,
+        {
+          foreignKey: 'userId'
+        })
     }
   }
   Attendance.init({
-    eventId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    status: DataTypes.ENUM
+    eventId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM({
+        values: ['attending', 'pending', 'waitlist']
+      }),
+      defaultValue: 'pending'
+    }
   }, {
     sequelize,
     modelName: 'Attendance',
