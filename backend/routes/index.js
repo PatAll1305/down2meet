427485bd01e2ecfd0bef7c567637fd5477156a30
Patrick Allen <patrickallen1305@gmail.com>
2024-08-25 11:40:26 -0500

    implemented backend route change to assist debug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,9 @@
+// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-
-// router.get("/api/csrf/restore", (req, res) => {
-//     const csrfToken = req.csrfToken();
-//     res.cookie("XSRF-TOKEN", csrfToken);
-//     res.status(200).json({
-//         'XSRF-Token': csrfToken
-//     });
-// });
 
 // Static routes
 // Serve React build files in production
