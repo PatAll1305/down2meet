6dd2ab9a206ddae4f0a9c4b78f70effe240a3a7f
Patrick Allen <patrickallen1305@gmail.com>
2024-09-02 11:24:08 -0500

    Bug fix attempt

--- frontend/src/components/Groups/GroupsById.jsx
+++ frontend/src/components/Groups/GroupsById.jsx
@@ -1,32 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Outlet } from "react-router-dom";
 import { allGroups } from "../../store/groups";
 import { Groups, GroupsView } from "./index";
 import { allGroupEvents } from "../../store/currentGroup";
 import './Group.css';
-import { csrfFetch } from "../../store/csrf";
 
 export default function GroupsById() {
     const { groupId } = useParams();
     const user = useSelector(state => state.session.user);
     const groups = useSelector(state => state.groups);
+    const events = useSelector(state => state.groupEvents)
     const group = groups[+groupId] ? groups[+groupId] : {};
-    const [events, setEvents] = useState({})
     const dispatch = useDispatch();
 
-
     useEffect(() => {
-        const getEvents = async (groupId) => {
-            const blob = await (await csrfFetch(`/api/groups/${groupId}/events`)).json()
-            const events = blob.Events
-            setEvents(events)
-        }
         dispatch(allGroups());
         dispatch(allGroupEvents(groupId));
-        getEvents(groupId)
     }, [groupId, dispatch])
-    console.log(events)
 
     return (
         <div>
@@ -34,7 +25,7 @@ export default function GroupsById() {
                 ?
                 <div id='groups'>
                     <Groups group={group} user={user} />
-                    {/* <GroupsView group={group} events={Object.values(events)} />z */}
+                    <GroupsView group={group} events={Object.values(events)} />z
                 </div>
                 :
                 <div id='groups'>
